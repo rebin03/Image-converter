ae14aa71505477d4886664e068895c7d8800515e
rebin03 <muhammedrebin1794@gmail.com>
2022-03-05 20:15:26 +0530

    new updation

--- resizer.py
+++ resizer.py
@@ -3,7 +3,7 @@ import sys
 
 try:
     fullimageFileName = sys.argv[1]
-
+  
     operation = sys.argv[2]
 
     argument = sys.argv[3]
@@ -18,15 +18,20 @@ try:
 
         converted = im.convert('RGB')
 
-        converted.save(f'{imageFileName}t.{argument}')
+        converted.save(f'{imageFileName}out.{argument}')
 
 
     elif operation == '-resize':
+
         im = Image.open(fullimageFileName)
+
         r = argument.split('x')
+
         r_tuple = (int(r[0]), int(r[1]))
+
         out = im.resize(r_tuple)
-        out.save(f'output_{fullimageFileName}')
+
+        out.save(f'New_{fullimageFileName}')
 
 except:
     print('Usage')
